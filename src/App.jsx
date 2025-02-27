2cbf5265acc50d30917403164d5bed14f6c7eadf
Arvid Berlin <arvidberlinjobb@gmail.com>
2025-02-27 16:08:04 +0100

    Added generic css, also added header, footer and main into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Header from './components/sections/Header'
+import Footer from './components/sections/Footer'
+import Main from './components/sections/Main'
 
 function App() {
 
   return (
     <div className='wrapper'>
-      
+      <Header />
+      <Main />
+      <Footer />
     </div>
   )
 }
