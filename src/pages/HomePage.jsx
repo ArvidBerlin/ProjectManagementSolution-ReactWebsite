29c0d9e5e1525645b610f46f926277768d06eb53
Arvid Berlin <arvidberlinjobb@gmail.com>
2025-02-27 14:54:58 +0100

    Added functionality to be redirected to projects-page when accessing home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/projects')
+  }, [navigate])
+  
   return (
     <main>
         <div className='container'>
-
+          Home
         </div>
     </main>
   )
