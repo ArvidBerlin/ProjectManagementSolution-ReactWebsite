703777d01fb0215e0c2aeeb4f5075d98e5270a3a
Arvid Berlin <arvidberlinjobb@gmail.com>
2025-02-27 16:26:23 +0100

    Added content into header, footer and main, with route paths to all pages

--- src/components/sections/Main.jsx
+++ src/components/sections/Main.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from '../../pages/HomePage'
+import Projects from '../../pages/Projects'
+import Project from '../../pages/Project'
+import CreateProject from '../../pages/CreateProject'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:id" element={<Project />} />
+      <Route path="/projects/create" element={<CreateProject />} />
+    </Routes>
   )
 }
 
